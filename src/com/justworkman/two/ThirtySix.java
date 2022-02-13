f2d1b36849883f63e4f641be157e9e38abb4493a
mouseonmars <mmssikg@gmail.com>
2022-02-13 16:34:13 +0500

    AfterWeek

--- src/com/justworkman/two/ThirtySix.java
+++ src/com/justworkman/two/ThirtySix.java
@@ -1,6 +1,12 @@
 package com.justworkman.two;
 
+
+import java.util.Date;
 import java.util.Scanner;
+
+import static com.justworkman.otherres.ColorConsole.ANSI_CYAN;
+import static com.justworkman.otherres.ColorConsole.ANSI_YELLOW;
+
 
 public class ThirtySix {
 
@@ -14,6 +20,7 @@ public class ThirtySix {
         String name = scanner.nextLine();
         System.out.println("Where you go?");
         String nextPoint = scanner.nextLine();
-        System.out.println(ARRIVE + name + TO_CITY + nextPoint);
+        System.out.println(ANSI_YELLOW+ ARRIVE + name + TO_CITY + nextPoint + " at " + new Date());
+
     }
 }
